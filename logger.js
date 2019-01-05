cb55f86c9d96e0cfedab3103db31a97592e444d9
newtmex <brightomex@gmail.com>
2019-01-05 14:07:06 +0100

    fix/Date string

--- logger.js
+++ logger.js
@@ -1,10 +1,10 @@
 function Logger (){
   this.log = function (info){
-    return console.log(`${new Date().toLocaleDateString()}: ${info}`);
+    return console.log(`${new Date().toString()}: ${info}`);
   }
 
   this.error = function (info){
-    return console.error(`${new Date().toLocaleDateString()}: ${info}`);
+    return console.error(`${new Date().toString()}: ${info}`);
   }
 }
 module.exports = new Logger()
