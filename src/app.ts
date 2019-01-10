daa198204832f089a8fb40ffc728a07b708f727f
newtmex <brightomex@gmail.com>
2019-01-10 11:47:30 +0100

    feature/Waiting for db to connect before getting snapshot

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,18 @@ import { getSnapshot } from "./helpers";
 
 logger.log('Starting app\n\n');
 
-dbConnect('mongodb://localhost/risehash', { useNewUrlParser: true });
-
 // Select one of the nodes that their api is enabled
 // or use localhost if in production
 rise.nodeAddress = process.env.NODE_ENV == 'production' ?
   'http://localhost:5555' :
   OpenNodes.openNodes[OpenNodes.lastOpenNodes_index];
 
+let dbConnection = dbConnect('mongodb://localhost/risehash', { useNewUrlParser: true });
+dbConnection.then(res => {
+  logger.log('Database connected successfully.')
   // Begin
   getSnapshot();
+}).catch(err => {
+  logger.error('Failed to connect to database..');
+  process.exit();
+})
