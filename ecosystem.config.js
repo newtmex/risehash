b0b036d14b255ad0d762d5ed02e8b3d1cf83c862
newtmex <brightomex@gmail.com>
2019-01-10 23:17:51 +0100

    Optimised for pm2 deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps : [{
+    name: 'risehash_app',
+    script: './dist/app.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    autorestart: true,
+    watch: true,
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'rise',
+      host : '139.162.193.60',
+      ref  : 'origin/master',
+      repo : 'git@github.com:newtmex/risehash.git',
+      path : '/var/www/risehash',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
